0a3bb664e01abad0f47f5be961650ef3d5772fe7
Denis <dburkov05@bk.ru>
2019-01-11 19:46:08 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,4 @@
-﻿var reg = false;
-navigator.serviceWorker.getRegistrations().then(function(registrations) {
- for(let registration of registrations) {
-  var url = registration.active.scriptURL
-  if(url == 'https://dburkov05.github.io/sw.js'){
-	  reg = true
-  }
-} })
-if(!reg){
-// при регистрации указываем на js-файл с кодом serviceWorker’а
+﻿// при регистрации указываем на js-файл с кодом serviceWorker’а
 // получаем Promise объект
 navigator.serviceWorker.register(
    '/sw.js'
@@ -16,8 +7,8 @@ navigator.serviceWorker.register(
     console.log('ServiceWorker registration', registration);
     // строкой ниже можно прекратить работу serviceWorker’а
     //registration.unregister();
+	registration.update();
 }).catch(function(err) {
     throw new Error('ServiceWorker error: ' + err);
 });
-}
 
